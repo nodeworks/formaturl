7ff82dc9309eedbb5d5b58fe6f37537007cca091
Rob Lee <rob@spartansystems.co>
2013-08-20 16:02:24 -0400

    Initial push

---
+++ jquery.formatUrl-1.0.js
@@ -0,0 +1,35 @@
+/*
+ * jQuery Format URL
+ * Copyright (c) 2013 Rob Lee <http://www.spartansystems.co>
+ * licensed under the LGPL Version 3 license.
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+;(function ($) {
+  $.fn.formatUrl = function() {
+    $(this).live('focus', function () {
+      if ($(this).val().indexOf('http://') != 0) {
+        var val = $(this).val();
+        $(this).val('http://' + val);
+      }
+    });
+
+    $(this).live('blur', function () {
+      if ($(this).val().indexOf('http://') != 0) {
+        if ($(this).val() == '') {
+          $(this).val('');
+        }
+        else {
+          var val = $(this).val();
+          $(this).val('http://' + val);
+        }
+      }
+      else {
+        if ($(this).val() == 'http://') {
+          $(this).val('');
+        }
+      }
+    });
+
+    return this;
+  };
+})(jQuery);
